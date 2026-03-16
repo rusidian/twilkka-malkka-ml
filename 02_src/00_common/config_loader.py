b5997d329d7e07f5a5be5b902c9a523f4815ce6d
Gloveman <bule3306@naver.com>
2026-03-16 14:25:13 +0900

    refactor(src): add type hint and docstring

--- 02_src/00_common/config_loader.py
+++ 02_src/00_common/config_loader.py
@@ -3,7 +3,10 @@ import json
 
 ROOT = Path(__file__).resolve().parents[2]
 CONFIG_FOLDER = ROOT / "04_configs"
-def load_config(config_name):
+def load_config(config_name: str) -> dict:
+    """
+    json 설정 파일 로드
+    """
     config_path = CONFIG_FOLDER / config_name
     with open(config_path, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
--- 02_src/02_model/00_architectures/factory.py
+++ 02_src/02_model/00_architectures/factory.py
@@ -1,19 +1,21 @@
+from typing import Any
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
 class LRBuilder:
-    def build(self, params):
+    def build(self, params: dict) -> LogisticRegression:
         model = LogisticRegression(**params)
         return model
 
 class RFBuilder:
-    def build(self, params):
+    def build(self, params: dict) -> RandomForestClassifier:
         model = RandomForestClassifier(**params)
         return model
 
 class XGBBuilder:
-    def build(self, params):
+    def build(self, params: dict) -> XGBClassifier:
         model = XGBClassifier(**params)
         return model
 
@@ -26,7 +28,10 @@ class ModelFactory:
     }
 
     @classmethod
-    def create_model(cls, model_name, params):
+    def create_model(cls, model_name: str, params: dict) -> Any:
+        """
+        model name에 해당하는 모델에 params 적용하여 모델 객체 생성 후 반환
+        """
         builder = cls._builders.get(model_name)
         if not builder:
             raise ValueError(f"❌ 지원하지 않는 모델입니다: {model_name}")
